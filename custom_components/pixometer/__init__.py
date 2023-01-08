452cae0f467e2b28d57b0f6a1ec4c2eb0bc47d5a
myTselection <myTselection@gmail.com>
2023-01-08 23:13:39 +0100

    0.0.4

--- custom_components/pixometer/__init__.py
+++ custom_components/pixometer/__init__.py
@@ -6,7 +6,7 @@ from homeassistant.core import Config, HomeAssistant
 
 DOMAIN = "pixometer"
 NAME = "Pixometer"
-VERSION = "0.0.3"
+VERSION = "0.0.4"
 ISSUEURL = "https://github.com/myTselection/pixometer/issues"
 
 STARTUP = """
